7fe2bdca2a1b2087faf23b00e3ba38ac3873cbd8
Frank-D-19 <frandvid@gmail.com>
2021-05-24 22:06:46 -0700

    Update StoreIndicador.php

--- app/Http/Requests/StoreIndicador.php
+++ app/Http/Requests/StoreIndicador.php
@@ -23,6 +23,7 @@ class StoreIndicador extends FormRequest
      */
     public function rules()
     {
+        //Validaciones
         return [
             'nombre' => 'required|min:5',
             'definicion' => 'required|min:5',
